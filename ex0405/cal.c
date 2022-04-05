365502e3e6a86fbe448ea51ca2d5a92a8e78590a
june3011 <junnykee@naver.com>
2022-04-05 12:36:40 +0900

    2022-04-05

---
+++ ex0405/cal.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "add.h"
+
+void main(){
+	int a, b;
+// shift + v 블럭 지정 y복사 p붙여넣기
+
+	printf("insert a\n");
+	scanf("%d",&a);
+
+	
+	printf("insert b\n");
+	scanf("%d",&b);
+
+	add(a, b);
+}
